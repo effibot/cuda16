7938b72a779b35f73c7de628bf13a6629a9ca666
Effibot <andrea.efficace1@gmail.com>
2023-03-11 04:25:23 +0100

    init

---
+++ .devcontainer/config/.aliases.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Custom aliases for container internal user's shell
+
+alias zshconfig="code ~/.zshrc"
+alias ohmyzsh="code ~/.oh-my-zsh"
+alias ls="lsd --group-dirs=first -S"
+alias ll="ls -l --total-size -h"
+alias la="ll -a"
+alias lt="ls --tree --depth=1 "
+alias clc='clear'
+alias mambas='mamba env list'
+alias mambau='mamba update --all'
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # cuda16
 Create docker image based on Ubuntu16.04 Xenial with ZSH and Mamba (Anaconda) installed.
+This image should be created inside of VSCode with the Remote Container extension or It can be pulled with
+'''
+docker pull effibot/cuda-ubuntu16:latest
+'''
